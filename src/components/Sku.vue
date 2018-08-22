ff4fd0d1215f6a4c45c7dd74d29c3eb66d11cad9
sjf_123 <3121077319@qq.com>
2018-08-22 17:07:11 +0800

    update sku two!

--- src/components/Sku.vue
+++ src/components/Sku.vue
@@ -3,14 +3,44 @@
   <el-row>
     <el-col :span="24">
       <div class="grid-content bg-purple-dark"></div>
-      <el-form ref="form" :model="form" label-width="80px" style="float:left">
+      <el-form ref="form" :model="form" label-width="80px" >
         <el-form-item
            v-for="(domain, index) in form.domains"
+           :key="domain.key"
             prop="region">
-            <el-input placeholder="请输入规格" v-model="input3" style="width:250px">
-             <template slot="prepend">规格名</template>
-           </el-input>
-
+            <el-row type="flex" justify="start">
+              <el-input placeholder="请输入规格" v-model="domain.value" style="width:250px">
+               <template slot="prepend">规格名</template>
+               </el-input>
+               <v-if v-if="index > 0">
+                  <el-button style="margin-left:10px"  @click.prevent="removeDomain(domain)" icon="el-icon-delete"  type="danger" round>删除规格</el-button>
+                </v-if>
+                <el-checkbox style="margin-left:10px" v-if="index == 0" @change="handleCheck" v-model="specChecked">添加规格图片</el-checkbox>
+            </el-row>
+             <el-row type="flex" justify="start">
+               <el-tag
+                 :key="tag"
+                 v-for="tag in dynamicTags"
+                 closable
+                 :disable-transitions="false"
+                 @close="handleClose(tag)"
+                 :hit="true"
+                 size="medium"
+                 >
+                 {{tag}}
+               </el-tag>
+               <el-input
+                 class="input-new-tag"
+                 v-if="inputVisible"
+                 v-model="inputValue"
+                 ref="saveTagInput"
+                 size="small"
+                 @keyup.enter.native="handleInputConfirm"
+                 @blur="handleInputConfirm"
+                 >
+               </el-input>
+               <el-button v-else class="button-new-tag" icon="el-icon-plus" size="small" @click="showInput(index)">添加属性</el-button>
+             </el-row>
          </el-form-item>
          <el-button v-if="this.addNum <= 1"  @click="addSku" icon="el-icon-plus" type="primary" round>新增规格</el-button>
        </el-form>
@@ -25,12 +55,19 @@
   export default {
     data() {
       return {
+         clickTag: '-1',
+         specChecked:false,
          addNum: 0,
          form: {
            domains: [{
-             value: ''
-           }],
-       },
+             value: '',
+           },
+          ],
+         },
+         input3:"",
+         dynamicTags: [],
+         inputVisible: false,
+         inputValue: ''
       }
     },
     methods: {
@@ -38,11 +75,49 @@
          if (this.addNum <= 1) {
            this.form.domains.push({
              value: '',
-             key: Date.now()
+             key: Date.now(),
            });
+
+           //this.dynamicTags.push({value:'123'})
          }
          this.addNum = this.addNum + 1;
         },
+        handleClose(tag) {
+          this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+        },
+        showInput(tag) {
+          this.clickTag = tag;
+          console.log( JSON.stringify(this.clickTag, null, 4));
+          console.log( JSON.stringify(this.dynamicTags, null, 4));
+          this.inputVisible = true;
+          this.$nextTick(_ => {
+            this.$refs.saveTagInput.$refs.input.focus();
+          });
+        },
+
+      handleInputConfirm() {
+        let inputValue = this.inputValue;
+        if (inputValue) {
+          this.dynamicTags.push(inputValue);
+        }
+        this.inputVisible = false;
+        this.inputValue = '';
+      },
+      handleCheck(value){
+       console.log("check:"+value);
+       if (!value) {
+         this.ruleForm.fileList = [];
+       }
+     },
+     removeDomain(item) {
+       if(this.addNum > 0){
+         this.addNum = this.addNum - 1;
+         var index = this.form.domains.indexOf(item)
+         if (index !== -1) {
+           this.form.domains.splice(index, 1)
+         }
+       }
+     },
     }
   }
 </script>
@@ -72,5 +147,20 @@
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
+  }
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
   }
 </style>
